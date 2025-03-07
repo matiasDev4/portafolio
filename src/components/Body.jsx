a17d86f0a39bbec16a0e91d6d002c05e6c5430e8
Matias alvarez <107509747+matiasDev4@users.noreply.github.com>
2025-03-07 02:37:07 -0300

    Actualizar Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -29,7 +29,7 @@ export const Body = () => {
     {
         "name": "Menu online",
         "description": "Un menu online atractivo y a medida para cada lugar, dandole un poco mas de agilidad al cliente a la hora de elegir su comida/bebida. Teniendo la opcion de filtro por cantegorias y una barra de busqueda",
-        "lenguajes": ["react.svg, tailwindcss.svg"],
+        "lenguajes": ["react.png, tailwindcss.svg"],
         "img": "proyectos/menucard.png",
         "link": "menuonlinegn.netlify.app",
         "active": true
