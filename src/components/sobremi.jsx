0c3c9de41912d23ca26658614280288673d74cf1
Matias alvarez <107509747+matiasDev4@users.noreply.github.com>
2025-08-18 15:16:45 -0300

    Update sobremi.jsx

--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -3,7 +3,7 @@ import { lenguagesImages } from "../utils/proyects"
 export const Sobremi = () => {
     return (
         <>
-            <section className="w-80 h-auto my-8 lg:w-[1200px] mx-auto
+            <section className="w-70 h-auto my-8 lg:w-[1200px] mx-auto
             flex flex-col-reverse lg:flex-row lg:justify-around items-center lg:items-start gap-y-5">
                 <div className="w-auto px-2">
                     <div className="text-white">
