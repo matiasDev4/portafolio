25fbc701fb2d4202385b96fe09ba41bc47a7b5ae
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-08-18 14:12:21 -0300

    change about me

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,32 @@ import { Footer } from "./footer.jsx"
 import { Sobremi } from "./sobremi.jsx"
 
 
-export const  App = () => {
+export const App = () => {
 
     return (
         <>
-        <Header />
-        <Sobremi />
-        <div 
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 gap-x-2 md:gap-x-20 lg:gap-x-50 place-items-center px-2 items-start auto-rows-fr md:max-w-[750px] lg:max-w-[1100px] mx-auto">
-            {cards.map(item => 
-                <CardProyect 
-                key={item.name}
-                name={item.name}
-                description={item.description}
-                lenguajes={item.lenguajes}
-                image={item.img}
-                download={item.link}
-                github={item.link_repo}
-                active={item.active}
-                type={item.type}
-                />
-            )}
-        </div>
-        <Footer />
+            <Header />
+            <Sobremi />
+            <div className="md:max-w-[750px] lg:max-w-[1100px] mx-auto">
+                <h1 className="text-amber-300 font-semibold py-10 text-3xl">Proyectos</h1>
+                <div
+                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 gap-x-2 md:gap-x-30 lg:gap-x-50 place-items-center items-start auto-rows-fr px-5">
+                    {cards.map(item =>
+                        <CardProyect
+                            key={item.name}
+                            name={item.name}
+                            description={item.description}
+                            lenguajes={item.lenguajes}
+                            image={item.img}
+                            download={item.link}
+                            github={item.link_repo}
+                            active={item.active}
+                            type={item.type}
+                        />
+                    )}
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
